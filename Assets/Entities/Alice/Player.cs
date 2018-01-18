21b1a09d710f8e17d526c645dbee6e5044c68893
GERALD\Gerald <burkeg@goldmail.etsu.edu>
2018-01-17 21:25:46 -0500

    More Animation Tweaks

--- Assets/Entities/Alice/Player.cs
+++ Assets/Entities/Alice/Player.cs
@@ -28,103 +28,106 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") >= 0.05f)
+        if (!anim.GetBool("dead"))
         {
-            anim.SetBool("upIdle", false);
-            anim.SetBool("downIdle", false);
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0.5f, 0), Vector2.right, 0.05f);
-            if (hit.collider == null)
+            if (Input.GetAxis("Horizontal") >= 0.05f)
             {
-                anim.SetBool("walk", true);
-                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+                anim.SetBool("upIdle", false);
+                anim.SetBool("downIdle", false);
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0.5f, 0), Vector2.right, 0.05f);
+                if (hit.collider == null)
+                {
+                    anim.SetBool("walk", true);
+                    transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    Debug.Log("blocked");
+                }
+                facing = 0;
             }
-            else
+            if (Input.GetAxis("Horizontal") <= -0.05f)
             {
-                Debug.Log("blocked");
+                anim.SetBool("upIdle", false);
+                anim.SetBool("downIdle", false);
+                this.GetComponent<SpriteRenderer>().flipX = true;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(-0.5f, 0), Vector2.left, 0.05f);
+                if (hit.collider == null)
+                {
+                    anim.SetBool("walk", true);
+                    transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+                    Debug.Log("clear");
+                }
+                else
+                {
+                    Debug.Log("blocked");
+                }
+                facing = 1;
             }
-            facing = 0;
-        }
-        if (Input.GetAxis("Horizontal") <= -0.05f)
-        {
-            anim.SetBool("upIdle", false);
-            anim.SetBool("downIdle", false);
-            this.GetComponent<SpriteRenderer>().flipX = true;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(-0.5f, 0), Vector2.left, 0.05f);
-            if (hit.collider == null)
+            if (Input.GetAxis("Horizontal") == 0)
             {
-                anim.SetBool("walk", true);
-                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-                Debug.Log("clear");
+                anim.SetBool("walk", false);
             }
-            else
+            if (Input.GetAxis("Vertical") >= 0.05f)
             {
-                Debug.Log("blocked");
+                anim.SetBool("upIdle", true);
+                anim.SetBool("downIdle", false);
+                facing = 2;
             }
-            facing = 1;
-        }
-        if (Input.GetAxis("Horizontal") == 0)
-        {
-            anim.SetBool("walk", false);
-        }
-        if (Input.GetAxis("Vertical") >= 0.05f)
-        {
-            anim.SetBool("upIdle", true);
-            anim.SetBool("downIdle", false);
-            facing = 2;
-        }
-        if (Input.GetAxis("Vertical") <= -0.05f)
-        {
-            anim.SetBool("downIdle", true);
-            anim.SetBool("upIdle", false);
-            facing = 3;
-        }
+            if (Input.GetAxis("Vertical") <= -0.05f)
+            {
+                anim.SetBool("downIdle", true);
+                anim.SetBool("upIdle", false);
+                facing = 3;
+            }
 
-        if (Input.GetButtonDown("RangeAttack"))
-        {
-            anim.SetTrigger("shoot");
-            switch (facing)
+            if (Input.GetButtonDown("RangeAttack"))
             {
-                case (0):
-                    Projectile projR = Instantiate(projectile, transform.position + Vector3.right, Quaternion.identity);
-                    projR.GetComponent<Rigidbody2D>().AddForce(Vector2.right * projectileSpeed);
-                    break;
-                case (1):
-                    Projectile projL = Instantiate(projectile, transform.position + Vector3.left, Quaternion.identity);
-                    projL.GetComponent<Rigidbody2D>().AddForce(Vector2.left * projectileSpeed);
-                    break;
-                case (2):
-                    Projectile projUp = Instantiate(projectile, transform.position + Vector3.up, Quaternion.identity);
-                    projUp.GetComponent<Rigidbody2D>().AddForce(Vector2.up * projectileSpeed);
-                    break;
-                case (3):
-                    Projectile projDown = Instantiate(projectile, transform.position + Vector3.down, Quaternion.identity);
-                    projDown.GetComponent<Rigidbody2D>().AddForce(Vector2.down * projectileSpeed);
-                    break;
+                anim.SetTrigger("shoot");
+                switch (facing)
+                {
+                    case (0):
+                        Projectile projR = Instantiate(projectile, transform.position + new Vector3(0.5f, 0), Quaternion.identity);
+                        projR.GetComponent<Rigidbody2D>().AddForce(Vector2.right * projectileSpeed);
+                        break;
+                    case (1):
+                        Projectile projL = Instantiate(projectile, transform.position + new Vector3(-0.5f, 0), Quaternion.identity);
+                        projL.GetComponent<Rigidbody2D>().AddForce(Vector2.left * projectileSpeed);
+                        break;
+                    case (2):
+                        Projectile projUp = Instantiate(projectile, transform.position + Vector3.up, Quaternion.identity);
+                        projUp.GetComponent<Rigidbody2D>().AddForce(Vector2.up * projectileSpeed);
+                        break;
+                    case (3):
+                        Projectile projDown = Instantiate(projectile, transform.position + Vector3.down, Quaternion.identity);
+                        projDown.GetComponent<Rigidbody2D>().AddForce(Vector2.down * projectileSpeed);
+                        break;
+                }
             }
-        }
 
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.5f), Vector2.down, 0.5f);
-            if (hit.collider != null)
+            if (Input.GetButtonDown("Jump"))
             {
-                anim.SetBool("jump", true);
-                GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.5f), Vector2.down, 0.5f);
+                if (hit.collider != null)
+                {
+                    anim.SetBool("jump", true);
+                    GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
+                }
             }
-        }
 
-        if (Input.GetButtonDown("Taunt"))
-        {
-        anim.SetTrigger("taunt");
-        }
+            if (Input.GetButtonDown("Taunt"))
+            {
+                anim.SetTrigger("taunt");
+            }
 
-        if (health <= 0)
-        {
-            anim.SetTrigger("fall");
-            anim.SetBool("dead", true);
-            Invoke("Death", 100f * Time.deltaTime);
+            if (health <= 0)
+            {
+                anim.SetTrigger("fall");
+                anim.SetBool("dead", true);
+                Invoke("Death", 100f * Time.deltaTime);
+            }
         }
     }
 
---
+++ Assets/Entities/Tiles/Explosive.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Explode", Random.Range(10, 20));
+	}
+	
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Projectile"))
+        {
+            Explode();
+        }
+    }
+
+    void Explode ()
+    {
+        RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, Random.Range(1,3));
+        RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, Random.Range(1, 3));
+        RaycastHit2D up = Physics2D.Raycast(transform.position, Vector2.up, Random.Range(1, 3));
+        RaycastHit2D down = Physics2D.Raycast(transform.position, Vector2.down, Random.Range(1, 3));
+        RaycastHit2D upLeft = Physics2D.Raycast(transform.position, new Vector2(-1,1), Random.Range(1, 3));
+        RaycastHit2D upRight = Physics2D.Raycast(transform.position, new Vector2(1,1), Random.Range(1, 3));
+        RaycastHit2D downLeft = Physics2D.Raycast(transform.position, new Vector2(-1,-1), Random.Range(1, 3));
+        RaycastHit2D downRight = Physics2D.Raycast(transform.position, new Vector2(-1,1), Random.Range(1, 3));
+
+        if (left.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(left.collider.gameObject);
+        } else if (left.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        } else if (left.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (right.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(right.collider.gameObject);
+        }
+        else if (right.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (right.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (up.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(up.collider.gameObject);
+        }
+        else if (up.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (up.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (down.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(down.collider.gameObject);
+        }
+        else if (down.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (down.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (upLeft.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(upLeft.collider.gameObject);
+        }
+        else if (upLeft.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (upLeft.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (upRight.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(upRight.collider.gameObject);
+        }
+        else if (upRight.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (upRight.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (downLeft.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(downLeft.collider.gameObject);
+        }
+        else if (downLeft.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (downLeft.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+        if (downRight.collider.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(downRight.collider.gameObject);
+        }
+        else if (downRight.collider.gameObject.CompareTag("Player1"))
+        {
+            FindObjectOfType<Player>().health = 0;
+        }
+        else if (downRight.collider.gameObject.CompareTag("Player2"))
+        {
+            FindObjectOfType<Player2>().health = 0;
+        }
+    }
+}
